3d6ffaec09a9b4b7e6c412725560438d2583746b
Chel-ovechek <145286085+Chel-ovechek@users.noreply.github.com>
2025-04-09 20:54:39 +0300

    Add files via upload

---
+++ Laboratory3/Character.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {}
+
+    // Перегрузка оператора ==
+    bool operator==(const Character& other) const {
+        return name == other.name && health == other.health;
+    }
+
+    // Перегрузка оператора <<
+    friend std::ostream& operator<<(std::ostream& os, const Character& character) {
+        os << "Character: " << character.name << ", HP: " << character.health
+            << ", Attack: " << character.attack << ", Defense: " << character.defense;
+        return os;
+    }
+};
+
+
+
---
+++ Laboratory3/Weapon.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class Weapon {
+private:
+    std::string title;
+    int damage;
+
+
+public:
+    Weapon(const std::string& t, int d)
+        : title(t), damage(d) {}
+
+    // Перегрузка оператора +
+    Weapon operator+(const Weapon& other) {
+        Weapon a("new_weapon", this->damage + other.damage);
+
+        return a;
+    }
+
+    bool operator>(const Weapon& other) {
+        return damage>other.damage;
+    }
+
+    // Перегрузка оператора <<
+    friend std::ostream& operator<<(std::ostream& os, const Weapon& weapon) {
+        os << "Weapon: " << weapon.title << ", Damage: " << weapon.damage;
+        return os;
+    }
+};
+
