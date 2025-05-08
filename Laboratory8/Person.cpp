69538a0a932483584a35b26b24693ced50145986
Chel-ovechek <145286085+Chel-ovechek@users.noreply.github.com>
2025-05-08 17:19:25 +0300

    Add files via upload

---
+++ Laboratory8/Person.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include "Person.h"
+
+// Реализация геттеров
+std::string Person::getName() const {
+    return name;
+}
+
+int Person::getAge() const {
+    return age;
+}
+
+std::string Person::getEmail() const {
+    return email;
+}
+
+std::string Person::getAddress() const {
+    return address;
+}
+
+// Реализация сеттеров
+void Person::setName(const std::string& newName) {
+    if (!newName.empty()) {
+        name = newName;
+    }
+    else {
+        std::cerr << "Error: Name cannot be empty!" << std::endl;
+    }
+}
+
+void Person::setAge(int newAge) {
+    if (newAge >= 0 && newAge <= 120) {
+        age = newAge;
+    }
+    else {
+        std::cerr << "Error: Age must be between 0 and 120!" << std::endl;
+    }
+}
+
+void Person::setEmail(const std::string& newEmail) {
+    if (newEmail.find('@') != std::string::npos) {
+        email = newEmail;
+    }
+    else {
+        std::cerr << "Error: Invalid email format!" << std::endl;
+    }
+}
+
+void Person::setAddress(const std::string& newAddress) {
+    if (!newAddress.empty()) {
+        address = newAddress;
+    }
+    else {
+        std::cerr << "Error: Address cannot be empty!" << std::endl;
+    }
+}
+
+// Реализация метода displayInfo
+void Person::displayInfo() const {
+    std::cout << "Name: " << name << ", Age: " << age << ", Email: " << email << ", Address: " << address << std::endl;
+}
---
+++ Laboratory8/Person.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+
+class Person {
+private:
+    std::string name;
+    int age;
+    std::string email;
+    std::string address;
+
+public:
+    // Геттеры
+    std::string getName() const;
+    int getAge() const;
+    std::string getEmail() const;
+    std::string getAddress() const;
+
+    // Сеттеры
+    void setName(const std::string& newName);
+    void setAge(int newAge);
+    void setEmail(const std::string& newEmail);
+    void setAddress(const std::string& newAddress);
+
+    // Метод для вывода информации
+    void displayInfo() const;
+};
+
