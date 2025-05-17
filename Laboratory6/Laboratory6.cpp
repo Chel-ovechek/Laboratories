5075b99a02ad64beb6bd7dae133604b851b80c7a
Chel-ovechek <145286085+Chel-ovechek@users.noreply.github.com>
2025-05-17 10:11:29 +0300

    Add files via upload

---
+++ Laboratory6/Laboratory6.cpp
@@ -0,0 +1,30 @@
+﻿#include "Queue.h"
+
+int main() {
+    setlocale(LC_ALL, "");
+    Queue<int> q;
+
+    // Проверка вызова pop() при пустой очереди
+    try {
+        std::cout << "Пытаемся извлечь элемент из пустой очереди..." << std::endl;
+        int value = q.pop(); // Это вызовет исключение
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "Обработка исключения: " << e.what() << std::endl;
+    }
+
+    // Теперь добавим элементы и попробуем снова
+    q.push(10);
+    q.push(20);
+    q.display();
+
+    try {
+        std::cout << "Извлечённый элемент: " << q.pop() << std::endl;
+        q.display();
+        q.pop();
+        q.pop(); // Еще раз, чтобы вызвать исключение
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "Обработка исключения: " << e.what() << std::endl;
+    }
+}
